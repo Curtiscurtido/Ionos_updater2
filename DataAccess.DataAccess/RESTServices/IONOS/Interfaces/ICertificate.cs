a5b3b2adf4b59c059035cab194c6e36eee898509
Curtiscurtido <157171271+Curtiscurtido@users.noreply.github.com>
2024-01-20 15:56:06 +0100

    Update ICertificate.cs

--- DataAccess.DataAccess/RESTServices/IONOS/Interfaces/ICertificate.cs
+++ DataAccess.DataAccess/RESTServices/IONOS/Interfaces/ICertificate.cs
@@ -1,9 +1,9 @@
 ﻿using DataAccess.Entities.IONOSEntities.Certificates;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq;   
+using System.Text;   
+using System.Threading.Tasks;   
 
 namespace DataAccess.DataAccess.RESTServices.IONOS.Interfaces
 {
